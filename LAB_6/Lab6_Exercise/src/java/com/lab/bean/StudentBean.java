e07cc81178c9356bb2d8c6c300817f86a4ab059b
sho <109146811+sho-exe@users.noreply.github.com>
2026-05-12 22:37:21 +0800

    Add files via upload

---
+++ LAB_6/Lab6_Exercise/src/java/com/lab/bean/StudentBean.java
@@ -0,0 +1,28 @@
+package com.lab.bean;
+
+public class StudentBean {
+
+    private String name;
+    private String matricNo;
+
+    public StudentBean() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMatricNo() {
+        return matricNo;
+    }
+
+    public void setMatricNo(String matricNo) {
+        this.matricNo = matricNo;
+
+    }
+
+}
